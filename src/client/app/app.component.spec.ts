734d3b6e74f7c6190b6bb2dfa2edd7a9d7112046
lexas <lexas.gummybears@gmail.com>
2016-10-06 15:36:54 +0200

    add search component

--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -14,7 +14,6 @@ import {
 } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { WeatherListComponent } from './weather/weather.component';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -26,7 +25,6 @@ export function main() {
 
     let config: Route[] = [
       { path: '', component: HomeComponent },
-      { path: 'about', component: AboutComponent }
       { path: 'weather', component: WeatherListComponent }
     ];
     beforeEach(() => {
@@ -34,7 +32,7 @@ export function main() {
         imports: [FormsModule, RouterTestingModule.withRoutes(config), MaterialModule.forRoot()],
         declarations: [TestComponent, ToolbarComponent,
           NavbarComponent, AppComponent,
-          HomeComponent, AboutComponent, WeatherListComponent],
+          HomeComponent, WeatherListComponent],
         providers: [
           { provide: APP_BASE_HREF, useValue: '/' }
         ]
