b2ef1b3328c3645c81f80b1e04baca13c3c0094e
lexas <lexas.gummybears@gmail.com>
2016-10-11 16:05:49 +0200

    add weather template/assets/service

--- src/client/app/about/about.component.ts
+++ src/client/app/about/about.component.ts
@@ -1,5 +1,4 @@
-
-import { Component }        from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 //import { JSONP_PROVIDERS }  from '@angular/http';
 import { Observable }       from 'rxjs/Observable';
 import { Subject }          from 'rxjs/Subject';
@@ -8,9 +7,7 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
 
-
 import { WeatherService } from './weather.service';
-
 
 @Component({
   moduleId: module.id,
@@ -26,11 +23,13 @@ export class AboutComponent {
  	constructor (private weatherService: WeatherService) {}
 
  	search(term: string) {this.isVisible = false;  this.searchTermStream.next(term); }
+  convert(temp: number) { return ((temp -32)*5/9).toFixed(0); }
 
  	items = this.searchTermStream
      .debounceTime(1000)
      .distinctUntilChanged()
      .switchMap((term: string) => this.weatherService.search(term))
-     .subscribe(data => {this.isVisible = true; console.log(data); this.items =  data});;
+     .subscribe(data => {this.isVisible = true; console.log(data); this.items =  data});
+
 
  }
--- src/client/app/about/about.module.ts
+++ src/client/app/about/about.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MdCardModule } from '@angular2-material/card';
 import { MdRadioModule } from '@angular2-material/radio';
 import { AboutComponent } from './about.component';
-
+import { MdInputModule } from '@angular2-material/input';
 
 @NgModule({
-    imports: [CommonModule, MdCardModule, MdRadioModule],
+    imports: [CommonModule, MdCardModule, MdRadioModule, MdInputModule],
     declarations: [AboutComponent],
     exports: [AboutComponent]
 })
