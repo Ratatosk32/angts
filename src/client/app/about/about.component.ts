7ff8393eae0034e5ca86797e16ec04f31969ef39
lexas <lexas.gummybears@gmail.com>
2016-10-11 14:56:45 +0200

    add weather template/assets/service

--- src/client/app/about/about.component.ts
+++ src/client/app/about/about.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
 
-/**
- * This class represents the lazy loaded AboutComponent.
- */
+import { Component }        from '@angular/core';
+//import { JSONP_PROVIDERS }  from '@angular/http';
+import { Observable }       from 'rxjs/Observable';
+import { Subject }          from 'rxjs/Subject';
+
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+
+
+import { WeatherService } from './weather.service';
+
+
 @Component({
   moduleId: module.id,
   selector: 'sd-about',
   templateUrl: 'about.component.html',
   styleUrls: ['about.component.css'],
+  providers: [WeatherService],
 })
-export class AboutComponent { }
+export class AboutComponent {
+ 	isVisible: boolean = false;
+ 	private searchTermStream = new Subject<string>();
+
+ 	constructor (private weatherService: WeatherService) {}
+
+ 	search(term: string) {this.isVisible = false;  this.searchTermStream.next(term); }
+
+ 	items = this.searchTermStream
+     .debounceTime(1000)
+     .distinctUntilChanged()
+     .switchMap((term: string) => this.weatherService.search(term))
+     .subscribe(data => {this.isVisible = true; console.log(data); this.items =  data});;
+
+ }
--- src/client/app/about/about.module.ts
+++ src/client/app/about/about.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MdCardModule } from '@angular2-material/card';
+import { MdRadioModule } from '@angular2-material/radio';
 import { AboutComponent } from './about.component';
 
 
 @NgModule({
-    imports: [CommonModule, MdCardModule],
+    imports: [CommonModule, MdCardModule, MdRadioModule],
     declarations: [AboutComponent],
     exports: [AboutComponent]
 })
