8fa86f756b2732fdcd020fbc7eb05ee423056b4a
lexas <lexas.gummybears@gmail.com>
2016-10-18 12:38:54 +0200

    minor changes + fix for page refresh :false might be changed on type=button for links

--- src/client/app/weather/reddit-article.ts
+++ src/client/app/weather/reddit-article.ts
@@ -4,44 +4,28 @@ import { Component } from '@angular/core';
 @Component({
  selector: 'reddit-article',
  host: {
- class: 'row'
+   class: 'row'
  },
  template: `
- <div class="four wide column center aligned votes">
- <div class="ui statistic">
- <div class="value">
- {{ votes }}
- </div>
- <div class="label">
- Points
- </div>
- </div>
- </div>
- <div class="twelve wide column">
- <a class="ui large header" href="{{ link }}">
- {{ title }}
- </a>
- <ul class="ui big horizontal list voters">
- <li class="item">
- <a href (click)="voteUp()">
- <i class="arrow up icon"></i>
- upvote
- </a>
- </li>
- <li class="item">
- <a href (click)="voteDown()">
- <i class="arrow down icon"></i>
- downvote
- </a>
- </li>
- </ul>
- </div>
- `
- })
+     <div class="four wide column center aligned votes">
+       <div class="ui statistic">
+         <div class="value">{{ votes }}</div>
+         <div class="label">Points</div>
+       </div>
+     </div>
+     <div class="twelve wide column">
+       <a class="ui large header" href="{{ link }}">{{ title }}</a>
+       <ul class="ui big horizontal list voters">
+         <li class="item"><a href (click)="voteUp(); false"><i class="arrow up icon"></i>upvote</a></li>
+         <li class="item"><a href (click)="voteDown(); false"><i class="arrow down icon"></i>downvote</a></li>
+       </ul>
+     </div>`
+     })
+
 export class ArticleComponent {
- votes: number;
- title: string;
- link: string;
+  votes: number;
+  title: string;
+  link: string;
 
 constructor() {
   this.title = 'Angular 2';
